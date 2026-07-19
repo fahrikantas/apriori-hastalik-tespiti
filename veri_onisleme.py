0a4096f5bba5a56bfe208b38ed5b9625034bc526
Fahri Kantas <kantasfahri@gmail.com>
2026-07-19 23:56:04 +0300

    Buraya ne kod yazdigini kisaca acikla

--- veri_onisleme.py
+++ veri_onisleme.py
@@ -1,12 +1,12 @@
 import pandas as pd
 print("Apriori projesi basliyor!")
-<<<<<<< HEAD
+
+print("yaptık laa")
+
+
+
 
 
 
 print("Veri onisleme basliyor!")
-=======
-print("Fahrİ Kantas")
-print("Merhaba")
-print("Adamsın")
->>>>>>> fb2a9840950d149784e827584b72b68838e4bdb7
+print("başlaa")
